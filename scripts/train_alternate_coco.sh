5b25569defc6bb0c83aaf1c72961261bd5ee5d8c
davidfu <lexuszhi1990@gmail.com>
2018-04-16 17:05:27 +0800

    add altenate coco shell

---
+++ scripts/train_alternate_coco.sh
@@ -0,0 +1,22 @@
+export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
+export PYTHONUNBUFFERED=1
+export MXNET_ENABLE_GPU_P2P=0
+# export PYTHONPATH=${PYTHONPATH}:incubator-mxnet/python/
+
+TRAIN_DIR=/mnt/models/mx-maskrcnn/v1
+DATASET=coco
+SET=train2017
+TEST_SET=val2017
+mkdir -p ${TRAIN_DIR}
+
+# Train
+python3.6 train_alternate_mask_fpn.py \
+    --network resnet_fpn \
+    --dataset ${DATASET} \
+    --image_set ${SET} \
+    --root_path ${TRAIN_DIR} \
+    --prefix ${TRAIN_DIR} \
+    --pretrained ./models/final \
+    --pretrained_epoch 0 \
+    --gpu 0 |& tee -a ${TRAIN_DIR}/train.log
+
